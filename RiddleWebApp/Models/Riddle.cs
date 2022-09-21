37a18a19c891be5a10cb08ebf3bb5b40e2b3f7f5
Serhii Zakutko <serhii.zakutko@nixsolutions.com>
2022-09-21 11:07:07 +0200

    Initial commit

---
+++ RiddleWebApp/Models/Riddle.cs
@@ -0,0 +1,13 @@
+﻿namespace RiddleWebApp.Models
+{
+    public class Riddle
+    {
+        public int Id { get; set; }
+        public string RiddleQuestion { get; set; }
+        public string RiddleAnswer { get; set; }
+        public Riddle()
+        {
+
+        }
+    }
+}
